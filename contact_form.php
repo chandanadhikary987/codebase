c8622d63128d776e3d6675e13cb0564d2fdbdd84
chandan.adhikary@codeclouds.in <chandan.adhikary@codeclouds.in>
2021-03-23 16:48:49 +0530

    messages

--- contact_form.php
+++ contact_form.php
@@ -11,26 +11,7 @@ Author URI: https://bestwebsoft.com/
 License: GPLv2 or later
 */
 
-/*  @ Copyright 2021 BestWebSoft  ( https://support.bestwebsoft.com )
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as
-	published by the Free Software Foundation.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-/**
-* Add Wordpress page 'bws_panel' and sub-page of this plugin to admin-panel.
-* @return void
-*/
 if ( ! function_exists( 'cntctfrm_admin_menu' ) ) {
 	function cntctfrm_admin_menu() {
 		global $submenu, $cntctfrm_plugin_info, $wp_version;
